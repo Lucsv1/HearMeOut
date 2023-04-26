b48b6350f3899959839fccae89d352c41ebe1c60
Lucas <lcasgalindo@gmail.com>
2023-04-26 10:07:38 -0300

    inciando telas

--- telas/App.js
+++ telas/App.js
@@ -1,20 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput } from "react-native";
+import imgLogo from "./assets/LOGO.webp";
+import { Colors } from "react-native/Libraries/NewAppScreen";
+
+const Formulario = (props) => {
+  return (
+    <View style={styles2.main}>
+      <View style={styles2.container}>
+        <Text style={styles2.text1Container}>HearMe</Text>
+        <Text style={styles2.text2Container}>Out</Text>
+      </View>
+      <View style={styles2.container2}>
+        <Text>Usuario</Text>
+        <TextInput/>
+        <Text>Senha</Text>
+        <TextInput/>
+      </View>
+    </View>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.main}>
+      <View style={styles.container}>
+        <Image source={imgLogo} style={styles.imgStyle} />
+      </View>
+      <View style={styles.container2}>
+        <Formulario />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#FFBD59",
+    alignItems: "center",
   },
+  imgStyle: {
+    marginTop: 30,
+    height: 50,
+  },
+  container2: {
+    flex: 8,
+    backgroundColor: "#FFEEEE",
+  },
+});
+
+const styles2 = StyleSheet.create({
+  main: {
+    flex: 1,
+  },
+  container:{
+    flex:1,
+    flexDirection: 'row',
+    justifyContent:'center',
+    marginTop: 30,
+  },
+  text1Container: {
+    fontSize: 40,
+
+  },
+  text2Container:{
+    fontSize: 40,
+    color: "#FFBD59"
+  },
+  container2:{
+    flex: 4,
+  }
 });
