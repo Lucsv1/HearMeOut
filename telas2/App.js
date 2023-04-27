02e7cb92ebd16ae033c0a9456bc994347c84c53d
Lucas <lcasgalindo@gmail.com>
2023-04-27 10:11:35 -0300

    segunda tela finalizada

--- telas2/App.js
+++ telas2/App.js
@@ -1,6 +1,9 @@
 import { StyleSheet, Text, View, Image } from "react-native";
 import imgLogo from "./assets/LOGO.webp";
 import hand from "./assets/hand.png";
+import imgRapaz from './assets/rapaz.png';
+import imgSeta from './assets/seta.png'
+
 
 const Conteudo = () => {
   return (
@@ -15,16 +18,20 @@ const Conteudo = () => {
         </Text>
       </View>
       <View style={styles2.container3}>
-        <View>
-          <Text>
+        <View style={styles2.content3}>
+          <Text style={styles2.textContent3}>
             Digitalizar processos sem perder a segurança é uma das prioridades
             do setor. A Plusoft reconhece esse desafio e oferece soluções que
             reúnem privacidade e experiência do cliente
           </Text>
-          <View>
-            <Text>Converse com nossos especialistas</Text>
+          <View style={styles2.botao}>
+            <Text style={styles2.botaoText}>Converse com nossos especialistas</Text>
           </View>
         </View>
+        <Image source={imgRapaz} style={styles2.imgContainer3} />
+      </View>
+      <View style={styles2.container4}>
+        <Image source={imgSeta} style={styles2.fotter} />
       </View>
     </View>
   );
@@ -34,9 +41,9 @@ export default function App() {
   return (
     <View style={styles.main}>
       <View style={styles.container}>
-        <Image source={imgLogo} style={styles2.imgContainer} />
+        <Image source={imgLogo} style={styles.imgContainer} />
       </View>
-      <View style={styles.container2}>
+      <View style={styles.container2} >
         <Conteudo />
       </View>
     </View>
@@ -55,7 +62,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   imgContainer: {
-    marginTop: 30,
+    marginTop: 35,
     height: 50,
   },
   container2: {
@@ -67,22 +74,18 @@ const styles = StyleSheet.create({
 const styles2 = StyleSheet.create({
   main2: {
     flex: 1,
-    borderWidth: 1,
   },
   container: {
-    flex: 1,
-    borderWidth: 1,
+    flex: 2,
     alignSelf: "center",
   },
   imgStyle: {
-    borderWidth: 1,
-    marginTop: 20,
+    marginTop: 50,
     height: 100,
     width: 100,
   },
   container2: {
     flex: 1,
-    borderWidth: 1,
   },
   textContainer2: {
     textAlign: "center",
@@ -93,7 +96,49 @@ const styles2 = StyleSheet.create({
     fontSize: 25,
   },
   container3: {
-    flex: 4,
+    flex: 3,
     backgroundColor: "#FFBD59",
+    flexDirection: 'row',
+    height: 200,
   },
+  content3:{
+   flexDirection: 'column',
+   width: '70%',
+   alignItems: 'center',
+  },
+  textContent3:{
+    textAlign: 'center',
+    fontSize: 25,
+    marginTop: 30
+
+
+  },
+  imgContainer3:{
+    width: 300,
+    height: 320,
+    position: 'absolute',
+    left: '45%',
+    bottom: 0.1
+  },
+  botao:{
+    borderWidth:1,
+    width: '70%',
+    marginTop: 40,
+    borderRadius: 10,
+    padding: 5
+  },
+  botaoText: {
+    textAlign: 'center',
+    fontWeight: 'bold'
+
+  },
+  container4:{
+    flex: 2,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  fotter:{
+    width: 100,
+    height: 100,
+  }
 });
